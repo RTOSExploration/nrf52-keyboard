5366270b7557d6eed34c7e695208fe2795e26607
Mingjie Shen <shen497@purdue.edu>
2023-04-29 20:41:41 +0000

    update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+cp template/Makefile.posix.template template/Makefile.posix
+
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export BINUTILS_TARGET_PREFIX=arm-none-eabi
+
 cd keyboard/lot60-ble
 make clean
-genbc /home/mjshen/RTOSExploration/bitcode-db/nrf52-keyboard -c "make -j$(nproc)"
+make -j$(nproc) CC=wllvm
+find _build/ -name "*.out" | xargs -n 1 extract-bc
